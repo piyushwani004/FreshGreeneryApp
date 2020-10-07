6d56a91c2a8c0bf0492c1b25fa5407d39fe7a978
piyushwani004 <piyushwani04@gmail.com.com>
2020-10-07 10:23:23 -0400

    Project Files

---
+++ ProjectFirst/app/src/main/java/com/piyush004/projectfirst/owner/messdetails/MessDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.piyush004.projectfirst.owner.messdetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.piyush004.projectfirst.R;
+
+public class MessDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mess_details);
+    }
+}
