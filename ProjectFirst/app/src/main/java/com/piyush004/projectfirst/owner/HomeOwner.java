2d6aad738fcba9d9ab7e94694bba573ef7d2d4af
piyushwani004 <piyushwani04@gmail.com.com>
2020-10-07 03:45:36 -0400

    Project Files

---
+++ ProjectFirst/app/src/main/java/com/piyush004/projectfirst/owner/HomeOwner.java
@@ -0,0 +1,23 @@
+package com.piyush004.projectfirst.owner;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.piyush004.projectfirst.R;
+
+public class HomeOwner extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.home_fragment_owner, container, false);
+
+    }
+
+}
