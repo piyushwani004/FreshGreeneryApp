167d31d961fe32022af451be4f71b171557ebd8a
piyushwani004 <piyushwani04@gmail.com.com>
2020-10-10 10:16:45 -0400

    Project Files

--- ProjectFirst/app/src/main/java/com/piyush004/projectfirst/owner/messdetails/MessDetailsActivity.java
+++ ProjectFirst/app/src/main/java/com/piyush004/projectfirst/owner/messdetails/MessDetailsActivity.java
@@ -1,11 +1,12 @@
 package com.piyush004.projectfirst.owner.messdetails;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,14 +18,13 @@ import androidx.appcompat.widget.Toolbar;
 import com.piyush004.projectfirst.Dashboard.OwnerDashboard;
 import com.piyush004.projectfirst.R;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public class MessDetailsActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ImageButton backButton;
-    private TextView textViewHeader;
+    private EditText editTextName, editTextAddress, editTextMobile, editTextEmail, editTextCity, editTextClosedDays;
+    private TextView textViewHeader, textViewName, textViewAddress, textViewMobile, textViewEmail, textViewCity, textViewClosedDays;
+    private Button buttonSave;
     private static int SELECT_PHOTO = 1;
     private ImageView imageView;
     private Uri uri;
@@ -38,6 +38,23 @@ public class MessDetailsActivity extends AppCompatActivity {
         backButton = findViewById(R.id.messdetailsBackBtn);
         textViewHeader = findViewById(R.id.messDetailsHeader);
         imageView = findViewById(R.id.messPhoto);
+
+        textViewName = findViewById(R.id.text_view_mess_name);
+        textViewAddress = findViewById(R.id.text_view_address);
+        textViewMobile = findViewById(R.id.text_view_mobile);
+        textViewEmail = findViewById(R.id.text_view_email);
+        textViewCity = findViewById(R.id.text_view_city);
+        textViewClosedDays = findViewById(R.id.text_view_closed_days);
+
+        editTextName = findViewById(R.id.edit_text_name);
+        editTextAddress = findViewById(R.id.edit_text_address);
+        editTextMobile = findViewById(R.id.edit_text_mobile);
+        editTextEmail = findViewById(R.id.edit_text_email);
+        editTextCity = findViewById(R.id.edit_text_city);
+        editTextClosedDays = findViewById(R.id.edit_text_closed_days);
+
+        buttonSave = findViewById(R.id.button_save);
+
     }
 
     public void onClickMessDetailsBackBtn(View view) {
@@ -52,11 +69,15 @@ public class MessDetailsActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == SELECT_PHOTO){
+        if (resultCode == RESULT_OK && requestCode == SELECT_PHOTO) {
             uri = data.getData();
             imageView.setImageURI(uri);
         }
+    }
+
+    public void onClickSaveEvent(View view) {
+
     }
 }
