e3913a4102fda988a50fea990ecd3d7c0666caaf
piyushwani004 <piyushwani04@gmail.com.com>
2020-10-15 05:53:28 -0400

    Project Files

--- ProjectFirst/app/src/main/java/com/piyush004/projectfirst/owner/messmenu/MessMenuActivity.java
+++ ProjectFirst/app/src/main/java/com/piyush004/projectfirst/owner/messmenu/MessMenuActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -31,8 +32,10 @@ public class MessMenuActivity extends AppCompatActivity {
     private String login_name;
     private FirebaseRecyclerOptions<MessMenuModel> options;
     private FirebaseRecyclerAdapter<MessMenuModel, MyListAdapter> adapter;
+    private EditText editTextName, editTextQuantity, editTextPrice;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mess_menu);
@@ -54,8 +57,9 @@ public class MessMenuActivity extends AppCompatActivity {
             public MessMenuModel parseSnapshot(@NonNull DataSnapshot snapshot) {
                 return new MessMenuModel(
                         snapshot.child("ItemName").getValue().toString(),
-                        snapshot.child("ItemQuantity").getValue().toString(),
-                        snapshot.child("ItemPrice").getValue().toString()
+                        snapshot.child("ItemPrice").getValue().toString(),
+                        snapshot.child("ItemQuantity").getValue().toString()
+
                 );
             }
         }).build();
@@ -102,32 +106,36 @@ public class MessMenuActivity extends AppCompatActivity {
         final View customLayout = getLayoutInflater().inflate(R.layout.mess_menu_form_dialog, null);
         builder.setView(customLayout);
 
+        editTextName = customLayout.findViewById(R.id.editTextMenuName);
+        editTextQuantity = customLayout.findViewById(R.id.editTextMenuUnit);
+        editTextPrice = customLayout.findViewById(R.id.editTextMenuPrice);
+
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                EditText editTextName = customLayout.findViewById(R.id.editTextMenuName);
-                EditText editTextQuantity = customLayout.findViewById(R.id.editTextMenuUnit);
-                EditText editTextPrice = customLayout.findViewById(R.id.editTextMenuPrice);
 
-                menuName = editTextName.getText().toString().trim();
-                menuQuantity = editTextQuantity.getText().toString().trim();
-                menuPrice = editTextPrice.getText().toString().trim();
+                menuName = editTextName.getText().toString();
+                menuQuantity = editTextQuantity.getText().toString();
+                menuPrice = editTextPrice.getText().toString();
 
-                sendDialogDataToActivity(menuName, menuQuantity, menuPrice);
+                databaseReference = FirebaseDatabase.getInstance().getReference().child("RegisterType").child(login_name).child("MessMenu");
+                String key = databaseReference.push().getKey();
+                databaseReference.child(key).child("ItemName").setValue(menuName);
+                databaseReference.child(key).child("ItemPrice").setValue(menuPrice);
+                databaseReference.child(key).child("ItemQuantity").setValue(menuQuantity);
+
+                Toast.makeText(MessMenuActivity.this, "Data Added To Database", Toast.LENGTH_SHORT).show();
+
+                editTextName.setText("");
+                editTextQuantity.setText("");
+                editTextPrice.setText("");
+
+                Intent intent = new Intent(MessMenuActivity.this, MessMenuActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
         AlertDialog dialog = builder.create();
         dialog.show();
     }
-
-    public void sendDialogDataToActivity(String name, String quan, String price) {
-
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("RegisterType").child(login_name).child("MessMenu");
-        String key = databaseReference.push().getKey();
-        databaseReference.child(key).child("ItemName").setValue(name);
-        databaseReference.child(key).child("ItemQuantity").setValue(quan);
-        databaseReference.child(key).child("ItemPrice").setValue(price);
-
-    }
-
 }
