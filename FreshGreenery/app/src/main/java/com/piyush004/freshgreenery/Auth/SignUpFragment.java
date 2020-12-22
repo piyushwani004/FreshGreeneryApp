e4ee5e60e49c2c5033f2edb77c37c847226988b2
piyushwani004 <piyushwani04@gmail.com.com>
2020-12-22 19:50:25 +0530

    Projects Files

--- FreshGreenery/app/src/main/java/com/piyush004/freshgreenery/Auth/SignUpFragment.java
+++ FreshGreenery/app/src/main/java/com/piyush004/freshgreenery/Auth/SignUpFragment.java
@@ -9,19 +9,14 @@ import androidx.fragment.app.Fragment;
 
 import com.piyush004.freshgreenery.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignUpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SignUpFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -29,14 +24,7 @@ public class SignUpFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static SignUpFragment newInstance(String param1, String param2) {
         SignUpFragment fragment = new SignUpFragment();
