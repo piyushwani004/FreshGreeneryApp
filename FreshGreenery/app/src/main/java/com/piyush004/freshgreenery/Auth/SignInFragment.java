1c1fe9a3c0fcc40a16415428bbc9c4e790057398
piyushwani004 <piyushwani04@gmail.com.com>
2020-12-22 19:35:50 +0530

    Projects Files

--- FreshGreenery/app/src/main/java/com/piyush004/freshgreenery/Auth/SignInFragment.java
+++ FreshGreenery/app/src/main/java/com/piyush004/freshgreenery/Auth/SignInFragment.java
@@ -1,12 +1,11 @@
 package com.piyush004.freshgreenery.Auth;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.piyush004.freshgreenery.R;
 
@@ -61,6 +60,12 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+
+
+
+        return view;
+
     }
 }
--- FreshGreenery/app/src/main/java/com/piyush004/freshgreenery/Auth/SignUpFragment.java
+++ FreshGreenery/app/src/main/java/com/piyush004/freshgreenery/Auth/SignUpFragment.java
@@ -1,12 +1,11 @@
 package com.piyush004.freshgreenery.Auth;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.piyush004.freshgreenery.R;
 
@@ -61,6 +60,10 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+
+
+        return view;
     }
 }
